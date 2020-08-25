0da4e73f6aba20480229904cd16d19b26bd82d81
Sepehr Tavakoli <sepehrtvk@aut.ac.ir>
2020-08-25 23:48:45 +0430

    Basic settings

---
+++ app.js
@@ -0,0 +1,44 @@
+// Budget Controller
+var budgetController = (function () {})();
+
+// UI Controller
+var UIController = (function () {
+  var DOMStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: '.add__btn'
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value,
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value,
+      };
+    },
+    getDOMStrings: function () {
+        return DOMStrings;
+    },
+  };
+})();
+
+// Controller
+var controller = (function (budgetCtrl, UICtrl) {
+
+    var DOMStrings = UICtrl.getDOMStrings();
+
+  var ctrlAddItem = function () {
+    var input = UICtrl.getInput();
+    console.log(input);
+  };
+
+  document.querySelector(DOMStrings.inputBtn).addEventListener("click", ctrlAddItem);
+
+  document.addEventListener("keypress", function (e) {
+    if (e.keyCode === 13 || e.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(budgetController, UIController);
